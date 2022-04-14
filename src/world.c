58f6eaa05f20ed4e08f16982528495f3880af8e6
Michael Naylor <66378426+mnaylor120@users.noreply.github.com>
2022-04-14 17:35:59 -0400

    Change comments to Doxygen friendly format

--- src/world.c
+++ src/world.c
@@ -22,23 +22,34 @@ void create_world(int p, int q, world_func func, void *arg) {
                 h = t;
                 w = 2;
             }
-            // sand and grass terrain
+                /**
+                * Logic to show sand & grass in game
+                */
             for (int y = 0; y < h; y++) {
                 func(x, y, z, w * flag, arg);
             }
             if (w == 1) {
                 if (SHOW_PLANTS) {
-                    // grass
+                    
+                /**
+                * Logic to show grass in game
+                */
                     if (simplex2(-x * 0.1, z * 0.1, 4, 0.8, 2) > 0.6) {
                         func(x, h, z, 17 * flag, arg);
                     }
-                    // flowers
+
+                    /**
+                    * Logic to show trees in game
+                     */                   
                     if (simplex2(x * 0.05, -z * 0.05, 4, 0.8, 2) > 0.7) {
                         int w = 18 + simplex2(x * 0.1, z * 0.1, 4, 0.8, 2) * 7;
                         func(x, h, z, w * flag, arg);
                     }
                 }
-                // trees
+
+                /**
+                * Logic to show trees in game
+                */
                 int ok = SHOW_TREES;
                 if (dx - 4 < 0 || dz - 4 < 0 ||
                     dx + 4 >= CHUNK_SIZE || dz + 4 >= CHUNK_SIZE)
@@ -62,7 +73,10 @@ void create_world(int p, int q, world_func func, void *arg) {
                     }
                 }
             }
-            // clouds
+            
+            /**
+             * logic to show clouds in game
+            */
             if (SHOW_CLOUDS) {
                 for (int y = 64; y < 72; y++) {
                     if (simplex3(
