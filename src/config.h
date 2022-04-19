a83c6f34a5cf649636e77d31bf79c224ca82eb1e
brandonhaire <haire.brandon116@gmail.com>
2022-04-19 16:19:29 -0400

    left hand controls now active

--- src/config.h
+++ src/config.h
@@ -28,9 +28,13 @@
 
 // key bindings
 #define CRAFT_KEY_FORWARD 'W'
+#define CRAFT_KEY_FORWARD2 'O'
 #define CRAFT_KEY_BACKWARD 'S'
+#define CRAFT_KEY_BACKWARD2 'L'
 #define CRAFT_KEY_LEFT 'A'
+#define CRAFT_KEY_LEFT2 'K'
 #define CRAFT_KEY_RIGHT 'D'
+#define CRAFT_KEY_RIGHT2 ';'
 #define CRAFT_KEY_JUMP GLFW_KEY_SPACE
 #define CRAFT_KEY_FLY GLFW_KEY_TAB
 #define CRAFT_KEY_OBSERVE 'O'
